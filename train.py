5fc1f767ff3d5819221548ee27f4d0d21a65417a
Sanghyeon Lee <lsh1107@kaist.ac.kr>
2022-03-14 15:15:48 +0900

    base code for training

--- train.py
+++ train.py
@@ -1,3 +1,25 @@
+import torch
+import torch.optim as optim
 
+# Dataset
+train_dataset = get_dataset()
+val_dataset = get_dataset()
 
-#### import dataset
+# Dataloader
+train_loader = torch.utils.data.DataLoader(train_dataset)
+val_loader = torch.utils.data.DataLoader(val_dataset)
+
+# Model
+model = get_model()
+
+# Optimizer
+optimizer = optim.Adam(model.parameters())
+
+# Trainer
+trainer = get_trainer(model, optimizer)
+
+# Training loop
+while True:
+    for batch in train_loader:
+        loss = trainer.train_step(batch)
+
