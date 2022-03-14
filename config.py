8c56be9456eb8e92484ec2153c7dff7711fb26bd
Sanghyeon Lee <lsh1107@kaist.ac.kr>
2022-03-14 16:53:42 +0900

    import template codes from occupancy flow

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
 import yaml
-import models
+import models, training
+import torch
+import torch.distributions as dist
+from torch import nn
 
 # General config
 def load_config(path):
@@ -9,26 +12,120 @@ def load_config(path):
 
     return cfg
 
+def get_decoder(cfg, device, dim=3, c_dim=0, z_dim=0):
+    
+    decoder = models.decoder.Decoder()
+    return decoder
+
+def get_velocity_field(cfg, device, dim=3, c_dim=0, z_dim=0):
+    
+    velocity_field = models.velocity_field.VelocityField()
+    return velocity_field
+
+def get_encoder(cfg, device, dataset=None, c_dim=0):
+    
+    encoder = models.pointnet.ResnetPointnet()
+    return encoder
+
+def get_encoder_latent(cfg, device, c_dim=0, z_dim=0):
+
+    encoder_latent = models.encoder_latent.PointNet()
+    return encoder_latent
+
+def get_encoder_latent_temporal(cfg, device, c_dim=0, z_dim=0):
+
+    encoder_latent_temporal = models.encoder_latent.PointNet()
+    return encoder_latent_temporal
+
+def get_encoder_temporal(cfg, device, dataset=None, c_dim=0, z_dim=0):
+    
+    encoder_temporal = models.pointnet.ResnetPointnet()
+    return encoder_temporal
+
+def get_prior_z(cfg, device, **kwargs):
+    ''' Returns the prior distribution of latent code z.
+
+    Args:
+        cfg (yaml config): yaml config object
+        device (device): PyTorch device
+    '''
+    z_dim = cfg['model']['z_dim'] #TODO
+    p0_z = dist.Normal(
+        torch.zeros(z_dim, device=device),
+        torch.ones(z_dim, device=device)
+    )
+
+    return p0_z
+
+
 # Models
 def get_model(cfg, device=None, dataset=None):
-    model = models.OccupancyFlow()
+
+    # Shortcuts
+    dim = cfg['data']['dim']
+    z_dim = cfg['model']['z_dim']
+    c_dim = cfg['model']['c_dim']
+    input_type = cfg['data']['input_type']
+    ode_solver = cfg['model']['ode_solver']
+    ode_step_size = cfg['model']['ode_step_size']
+    use_adjoint = cfg['model']['use_adjoint']
+    rtol = cfg['model']['rtol']
+    atol = cfg['model']['atol']
+
+    decoder = get_decoder(cfg, device, dim, c_dim, z_dim)
+    velocity_field = get_velocity_field(cfg, device, dim, c_dim, z_dim)
+    encoder = get_encoder(cfg, device, dataset, c_dim)
+    encoder_latent = get_encoder_latent(cfg, device, c_dim, z_dim)
+    encoder_latent_temporal = get_encoder_latent_temporal(
+        cfg, device, c_dim, z_dim)
+    encoder_temporal = get_encoder_temporal(cfg, device, dataset, c_dim, z_dim)
+    p0_z = get_prior_z(cfg, device)
+
+
+    model = models.OccupancyFlow(decoder=decoder, encoder=encoder, encoder_latent=encoder_latent,
+        encoder_latent_temporal=encoder_latent_temporal,
+        encoder_temporal=encoder_temporal, vector_field=velocity_field,
+        ode_step_size=ode_step_size, use_adjoint=use_adjoint,
+        rtol=rtol, atol=atol, ode_solver=ode_solver,
+        p0_z=p0_z, device=device, input_type=input_type)
     return model
 
 
 # Trainer
 def get_trainer(model, optimizer, cfg, device):
-    
+
+    threshold = cfg['test']['threshold']
+    out_dir = cfg['training']['out_dir']
+    vis_dir = os.path.join(out_dir, 'vis')
+    input_type = cfg['data']['input_type']
+    loss_corr = cfg['model']['loss_corr']
+    loss_recon = cfg['model']['loss_recon']
+    loss_corr_bw = cfg['model']['loss_corr_bw']
+    eval_sample = cfg['training']['eval_sample']
+    vae_beta = cfg['model']['vae_beta']
+
+    trainer = training.Trainer(
+        model, optimizer, device=device, input_type=input_type,
+        vis_dir=vis_dir, threshold=threshold,
+        eval_sample=eval_sample, loss_corr=loss_corr,
+        loss_recon=loss_recon, loss_corr_bw=loss_corr_bw,
+        vae_beta=vae_beta)
+
     return trainer
 
 
 # Generator for final mesh extraction
 def get_generator(model, cfg, device):
-    
+
+    #TODO
+
     return generator
 
 
 # Datasets
 def get_dataset(mode, cfg, return_idx=False, return_category=False):
+
+    #TODO
 
     return dataset
 
--- models/encoder_latent.py
+++ models/encoder_latent.py
@@ -1,2 +1,87 @@
-class Encoder(nn.Module):
-    pass
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from layers.layers import ResnetBlockFC
+
+
+def maxpool(x, dim=-1, keepdim=False):
+    ''' Performs a maximum pooling operation.
+
+    Args:
+        x (tensor): input tensor
+        dim (int): dimension of which the pooling operation is performed
+        keepdim (bool): whether to keep the dimension
+    '''
+    out, _ = x.max(dim=dim, keepdim=keepdim)
+    return out
+
+class PointNet(nn.Module):
+    ''' Latent PointNet-based encoder class.
+
+    It maps the inputs together with an (optional) conditioned code c
+    to means and standard deviations.
+
+    Args:
+        dim (int): dimension of input points
+        z_dim (int): dimension of latent code z
+        c_dim (int): dimension of latent conditioned code c
+        hidden_dim (int): dimension of hidden size
+        n_blocks (int): number of ResNet-based blocks
+    '''
+
+    def __init__(self, z_dim=128, c_dim=128, dim=51, hidden_dim=128,
+                 n_blocks=3, **kwargs):
+        super().__init__()
+        self.c_dim = c_dim
+        self.dim = dim
+        self.n_blocks = n_blocks
+
+        self.fc_pos = nn.Linear(dim, 2*hidden_dim)
+
+        self.blocks = nn.ModuleList(
+            [ResnetBlockFC(2*hidden_dim, hidden_dim) for i in range(n_blocks)])
+
+        if self.c_dim != 0:
+            self.c_layers = nn.ModuleList(
+                [nn.Linear(c_dim, 2*hidden_dim) for i in range(n_blocks)])
+
+        self.actvn = nn.ReLU()
+        self.pool = maxpool
+
+        self.fc_mean = nn.Linear(hidden_dim, z_dim)
+        self.fc_logstd = nn.Linear(hidden_dim, z_dim)
+
+    def forward(self, inputs, c=None, **kwargs):
+        ''' Performs a forward pass through the network.
+
+        Args:
+            inputs (tensor): inputs
+            c (tensor): latent conditioned code c
+        '''
+        batch_size, n_t, T, _ = inputs.shape
+
+        # Reshape input is necessary
+        if self.dim == 3:
+            inputs = inputs[:, 0]
+        else:
+            inputs = inputs.transpose(
+                1, 2).contiguous().view(batch_size, T, -1)
+        # output size: B x T X F
+        net = self.fc_pos(inputs)
+
+        for i in range(self.n_blocks):
+            if self.c_dim != 0:
+                net_c = self.c_layers[i](c).unsqueeze(1)
+                net = net + net_c
+
+            net = self.blocks[i](net)
+            if i < self.n_blocks - 1:
+                pooled = self.pool(net, dim=1, keepdim=True).expand(net.size())
+                net = torch.cat([net, pooled], dim=2)
+
+        net = self.pool(net, dim=1)
+
+        mean = self.fc_mean(net)
+        logstd = self.fc_logstd(net)
+
+        return mean, logstd
