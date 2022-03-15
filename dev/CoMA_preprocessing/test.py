454c99e4a22e7b8f139c6d7eebb890efa02d20ef
SeungminHwang <hwangtmdals@gmail.com>
2022-03-15 16:50:09 +0900

    fix: coordinate calc. and visual tool change

--- dev/CoMA_preprocessing/test.py
+++ dev/CoMA_preprocessing/test.py
@@ -3,7 +3,8 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 import util
-
+import open3d as o3d
+from tqdm import tqdm
 
 
 
@@ -41,14 +42,17 @@ print(x)
 test_obj.get_face_plane_coeff(1)
 
 
-
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.view_init(-90, 90)
+# SYYI commented out
+#fig = plt.figure()
+#ax = fig.add_subplot(projection='3d')
+#ax.view_init(-90, 90)
 
 
 print("face sampling start")
-for i in range(1, len(test_obj.f)):
+points = np.zeros((0,3)) # SYYI appended
+colors = np.zeros((0,3)) # SYYI appended
+#for i in range(1, len(test_obj.f)): # SYYI commented out
+for i in tqdm(range(1, len(test_obj.f))): # SYYI appended
     x, c = test_obj.get_points_on_face(i)
     #print(x)
     if len(x) == 0:
@@ -57,13 +61,29 @@ for i in range(1, len(test_obj.f)):
     Y = x[:, 1]
     Z = x[:, 2]
     
+    # SYYI appended #
+    points = np.concatenate([points, x])
+    colors = np.concatenate([colors, c])
     #print(c)
-    ax.scatter(X, Y, Z, c = c, alpha=0.3)
+    #ax.scatter(X, Y, Z, c = c, alpha=0.6, linewidths=0.8)
+    # SYYI end #
+    
 
     #vt1, vt2, vt3 = test_obj.get_face_vt(i)
+# SYYI appended #
+print("points.shape=%s" % (points.shape,))
+print("points[0]=%s" % points[0])
+print("colors.shape=%s" % (colors.shape,))
+print("colors[0]=%s" % colors[0])
 
+pcd = o3d.geometry.PointCloud()
+pcd.points = o3d.utility.Vector3dVector(points)
+pcd.colors = o3d.utility.Vector3dVector(colors*2)
+pcd.normals = o3d.utility.Vector3dVector(points / np.expand_dims(np.linalg.norm(points, axis=1), axis=1))
 
+o3d.visualization.draw_geometries([pcd])
+# SYYI end
 
 #plt.imshow(test_obj.texture)
 
-plt.show()
+#plt.show()
